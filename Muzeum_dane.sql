3bc456abc1645119ca6930019cc681b741919a63
StachKazmierowski <36884939+StachKazmierowski@users.noreply.github.com>
2018-09-11 16:16:18 +0200

    Update Muzeum_dane.sql

--- Muzeum_dane.sql
+++ Muzeum_dane.sql
@@ -1,1 +1,5 @@
-insert into 
+insert into Artysta (id, imie, nazwisko, rokNarodzin, rokSmierci) values(1, 'Salvador', 'Dali', 1904, 1989);
+insert into Artysta (id, imie, nazwisko, rokNarodzin, rokSmierci) values(2, 'Rembrandt', 'van Rijn', 1606, 1669);
+insert into Artysta (id, imie, nazwisko, rokNarodzin, rokSmierci) values(3, 'Vincent', 'van Gogh', 1853, 1890);
+insert into Artysta (id, imie, nazwisko, rokNarodzin, rokSmierci) values(4, 'Pablo', 'Picasso', 1881, 1973);
+insert into Artysta (id, imie, nazwisko, rokNarodzin, rokSmierci) values(5, 'Claude', 'Monet', 1840, 1926);

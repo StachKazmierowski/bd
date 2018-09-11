0dc17a3e27cdec66d1866ce132ed5f55d5634224
StachKazmierowski <36884939+StachKazmierowski@users.noreply.github.com>
2018-09-11 23:17:54 +0200

    Update Muzeum_wyzwalacze.sql

--- Muzeum_wyzwalacze.sql
+++ Muzeum_wyzwalacze.sql
@@ -1,1 +1,36 @@
 
+--jedno dzieło w jednym miejcu w jednym czasie
+
+create or replace function f1 () returns trigger as $$
+
+  begin
+  
+    if (exists (select * from Ekspozycja where id != new.id and id id_dzielo = new.id_dzielo and (new.poczatek < koniec) or (new.koniec < poczatek))
+    then
+        raise exception 'Wskazany termin koliduje z inną ekspozycją tego dzieła.';
+    else
+        return new;
+    end if;
+  end;
+$$ language pgpsl;
+        
+drop trigger if exists t1 on Ekspozycja;
+create trigger t1 before insert or update
+    on Ekspozycja for each row
+    execute procedure f1();
+        
+-- liczba eksponatów nie przekracza pojemnosci sali
+        
+create or replace function f2 () returns trigger as $$
+        
+    begin
+        
+        if (new.id_objazd is not null)
+        then
+            return new;
+        end if;
+        
+        
+        
+        
+        

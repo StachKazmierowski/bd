15c266236b0936450abf54a4de33c1257f3cfd9d
StachKazmierowski <36884939+StachKazmierowski@users.noreply.github.com>
2018-09-12 12:21:31 +0200

    Update Muzeum_wyzwalacze.sql

--- Muzeum_wyzwalacze.sql
+++ Muzeum_wyzwalacze.sql
@@ -5,23 +5,29 @@ create or replace function f1 () returns trigger as $$
 
   begin
   
-    if (exists (select * from Ekspozycja where id != new.id and id id_dzielo = new.id_dzielo and (new.poczatek < koniec) or (new.koniec < poczatek))
+    if (exists (select * from Ekspozycja where id != new.id and id_dzielo = new.id_dzielo and (new.poczatek < koniec) or (new.koniec < poczatek)))
     then
-        raise exception 'Wskazany termin koliduje z inną ekspozycją tego dzieła.';
+        raise exception 'Wskazany termin koliduje z inną ekspozycją tego dzieła.' ;
     else
         return new;
     end if;
   end;
-$$ language pgpsl;
+$$ language plpgsql;
         
 drop trigger if exists t1 on Ekspozycja;
 create trigger t1 before insert or update
     on Ekspozycja for each row
     execute procedure f1();
         
+        
 -- liczba eksponatów nie przekracza pojemnosci sali
         
 create or replace function f2 () returns trigger as $$
+    
+    declare
+        dzien date := new.poczatek;
+        licznik integer := 0;
+        pojemnosc integer;
         
     begin
         
@@ -29,6 +35,8 @@ create or replace function f2 () returns trigger as $$
         then
             return new;
         end if;
+        
+        
         
         
         

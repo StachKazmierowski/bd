367769fa91ff8e6090c1e281c6365a91b8395f39
StachKazmierowski <36884939+StachKazmierowski@users.noreply.github.com>
2018-09-13 14:04:49 +0200

    Create app.php

---
+++ app.php
@@ -0,0 +1,13 @@
+<?php
+
+switch ($-GET["tabela"]){
+
+    echo "    <a href=\"?tabela=eksponaty\"> eksponaty </a>";
+
+    echo "    <a href=\"?tabela=artysci\"> artyści </a>";
+    
+    echo "    <a href=\"?tabela=galerie\"> galerie </a>";
+    
+    echo "    <a href=\"?tabela=objazdy\"> odjazdy </a>";
+    
+    }
